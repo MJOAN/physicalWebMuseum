d0621f100e3270c2c3f876b9ea4ee48b4f762f11
Mariam Joan <royalton@gmail.com>
2017-11-19 17:45:16 -0800

    11-19

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const gcloud = require("google-cloud");
 // const connection = require("./config/connection");
 
 const app = express();

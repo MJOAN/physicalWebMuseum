b1ca1a7675b165c7b532880e4fe3c391646b03b5
Mariam Joan <royalton@gmail.com>
2017-11-19 17:23:37 -0800

    11-19: schema, server, routes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,12 +4,12 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index"));
+    res.sendFile(path.join(__dirname, "main"));
   });
 
  
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index"));
+    res.sendFile(path.join(__dirname, "main"));
   });
 
 

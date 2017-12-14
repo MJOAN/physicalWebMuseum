a44bf4d52e7b1f00c2fcd5731a53ee60051b7f72
Alex Edward Ball <ballalexedward@gmail.com>
2017-12-13 19:22:43 -0800

    commenting out session

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const session = require('express-session');
 const passportconfig = require("./config/passport");
 
 app.use(cookieParser());
-app.use(session({secret: 'secret'}));
+// app.use(session({secret: 'secret'}));
 app.use(passport.initialize());
 app.use(passport.session());
 

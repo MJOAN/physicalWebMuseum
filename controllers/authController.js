d442ad84dcc15201d8ff71b2815f096fa6e39364
Mariam Joan <royalton@gmail.com>
2017-12-13 20:09:13 -0800

    12-13 - auth passport

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,15 +5,23 @@ const router = express.Router();
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require('bcrypt-nodejs');
+var logAuth;
 
-router.post("/login", passport.authenticate('local', { 
-  failureRedirect: '/',
-  successRedirect: '/settings'
-}))
+router.post("/login", passport.authenticate('local', 
+    { 
+        successRedirect: '/settings', 
+        failureRedirect: '/settings'
+    }));
+
+router.get("/login", function(req, res) {
+    res.render("login");
+    console.log("login")
+});
 
 router.post("/signup", function(req, res, next) {
- console.log(req.body.email);
-  console.log("route post working")
+    console.log(req.body.email);
+    console.log("hello", "promise to settings redirect")
+    console.log("route post working")
     db.User.findOne({
         where: {
             email: req.body.email
@@ -25,47 +33,20 @@ router.post("/signup", function(req, res, next) {
                     email: req.body.email,
                     password: bcrypt.hashSync(req.body.password)
             }).then(function(user) {
+                res.send({redirect: "/settings" });
             }).catch(function(err) {
                 res.json(err);
                 });
             } else {
-                 console.log("hello", email, "promise to settings redirect")
+                 console.log("promise to settings redirect")
                 res.send({redirect: "/settings" });
             }
         });
     });
 
-
-router.get("/login", function(req, res) {
-    res.render("login");
-    console.log("login")
-});
-
-
-/*router.get("/login", function(req, res) {
-    if (req.user) {
-      res.redirect("/members");
-    }  
-    else {
-    db.User.findAll({
-        where: {
-            email: req.body.email
-        }
-    }).then(dbUser => {
-        uidObj = {
-            email: dbUser,
-            pageTitle: 'Login Page',
-            customCSS: './..',
-            customJS: './javascript/login.js'
-        }
-    res.send("login", uidObj);
-  });
-*/
-
 router.get("/signup", function(req, res){
     console.log("signup")
     res.render("signup");
-
 });
 
 router.get("/logout", function(req, res) {
@@ -73,16 +54,17 @@ router.get("/logout", function(req, res) {
     res.redirect('/');
 });
 
-router.get("/profile", function(req, res) {    
-    function logAuth(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        res.direct('/profile', { email: req.email });
-    }
-    logAuth(req,res);
-};
 
+/*router.get('/settings', ensureAuthenticated, function(req, res) {
+  res.send("access granted. secure stuff happens here");
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next(); }
+  res.redirect('/')
+}*/
+
+
 
 module.exports = router;
